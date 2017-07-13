3ab4f212cbd24e255970ac9440bccbddfe62f60f
Zach Butcher <duke.fan04@yahoo.com>
2017-07-13 18:50:52 -0500

    initial upload

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var newLabel: UILabel!
+    
+    var buttoncounter = 0
+    
+    @IBAction func PushButton(_ sender: Any) {
+        newLabel.text = "Is the Best!"
+        newLabel.textColor = UIColor.red
+        
+        buttoncounter += 1
+        
+        if buttoncounter >= 10{
+            newLabel.text = "button pushed 10 times!"
+        }
+    }
+    
+    @IBAction func SecondButton(_ sender: Any) {
+        buttoncounter = 0
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        newLabel.text = "King James"
+        newLabel.textColor = UIColor.cyan
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
